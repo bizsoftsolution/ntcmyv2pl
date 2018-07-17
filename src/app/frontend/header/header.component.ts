321d0ef8248befb60c8f61f5fed92154f6578102
Raj Kumar <rajkumar.bizsoft@gmail.com>
2018-07-17 09:48:19 +0530

    changes frontend

--- src/app/frontend/header/header.component.ts
+++ src/app/frontend/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { LoginComponent } from '../../backend/login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  loginpopup() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '550px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      alert('Closed');
+    });
   }
 
 }
